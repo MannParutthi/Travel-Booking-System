c1967ce884baceb588d22b87f42a9bfe32a507fb
jwalit21 <jwalitshah2q@gmail.com>
2023-05-16 01:39:02 -0400

    Added Customer controller

---
+++ src/main/java/com/codeblooded/travelbookingsystem/travelbookingsystem/customer/CustomerController.java
@@ -0,0 +1,28 @@
+package com.codeblooded.travelbookingsystem.travelbookingsystem.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createCustomer(Customer customer) {
+        String response = customerService.createCustomer(customer);
+        if(response == CustomerService.CX_ALREADY_EXISTS) {
+            return new ResponseEntity<String>(response, HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<String>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Iterable<Customer>> getAllCustomers() {
+        return new ResponseEntity<Iterable<Customer>>(customerService.getAllCustomers(), HttpStatus.OK);
+    }
+}
