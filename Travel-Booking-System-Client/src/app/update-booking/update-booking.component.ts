796fe9e8e044b596a67178d3fbf2c8b747690f3a
jwalit21 <jwalitshah2q@gmail.com>
2023-05-17 18:03:46 -0400

    Added update booking

--- Travel-Booking-System-Client/src/app/update-booking/update-booking.component.ts
+++ Travel-Booking-System-Client/src/app/update-booking/update-booking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UpdateBookingService } from './update-booking.service';
 
 @Component({
   selector: 'app-update-booking',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateBookingComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup = this.formBuilder.group({
+    'id': [0, []],
+    'customerId': [null, []],
+    'travelPackageId': [null, []],
+    'departureDate': [null, []],
+    'bookingStatus': [null, []],
+  });
+
+  updateBookingAPIResponse: any;
+
+  allBookingsList: any[] = [];
+
+  displayedColumns: string[] = ['id', 'customerId', 'travelPackageId', 'departureDate', 'bookingStatus'];
+
+  constructor(private formBuilder: FormBuilder, private updateBookingService: UpdateBookingService) { }
 
   ngOnInit(): void {
+    this.getAllBookings();
+  }
+
+  updateBooking() {
+    console.log("createBookingFormGroup ==> " + JSON.stringify(this.formGroup.getRawValue()));
+    this.updateBookingService.updateBooking(this.formGroup.getRawValue()).subscribe((res) => {
+      console.log("updateBookingAPIResponse ==> " + res);
+      this.updateBookingAPIResponse = res;
+      this.getAllBookings();
+    });
+  }
+
+  getAllBookings() {
+    this.updateBookingService.getAllBookings().subscribe((res) => {
+      this.allBookingsList = res;
+      console.log("getAllBookings ==> " + res);
+    });
   }
 
 }
--- Travel-Booking-System-Client/src/app/update-booking/update-booking.service.ts
+++ Travel-Booking-System-Client/src/app/update-booking/update-booking.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateBookingService {
+  private baseUrl = "http://localhost:8081/api/v1";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  updateBooking(booking: any): Observable<any> {
+    let bookingId = booking.id;
+    return this.http.post(`${this.baseUrl}/bookings/update${bookingId}`, booking, { responseType: 'text' }) as Observable<any>;
+  }
+
+  getAllBookings(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/bookings/all`) as Observable<any>;
+  }
 }
